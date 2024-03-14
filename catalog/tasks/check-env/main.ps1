6559f3fd1108df5109a4ceee4bddff3e42ff811a
Developer 01 <dev01@mcap784488.ayuina.moe>
2024-03-14 14:39:47 +0900

    update custom task

--- catalog/tasks/check-env/main.ps1
+++ catalog/tasks/check-env/main.ps1
@@ -6,6 +6,15 @@ param(
 function Main {
     Write-Host "Hello $name !"
     Write-Host ""
+
+    Write-Host '=== Current Location ==='
+    Get-Location
+    Write-Host ""
+
+    Write-Host '=== PSScriptRoot ==='
+    $PSScriptRoot
+    Write-Host ""
+
     Write-Host '=== Environment Variables ==='
     Get-Item Env:
     Write-Host ""
