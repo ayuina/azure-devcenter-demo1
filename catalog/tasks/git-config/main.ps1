a77a092f181fd73eb5f7ce630e648b886aeca5a5
dev01 <dev01@mcap784488.ayuina.moe>
2024-03-15 17:15:47 +0900

    fix script

--- catalog/tasks/git-config/main.ps1
+++ catalog/tasks/git-config/main.ps1
@@ -4,6 +4,10 @@ param(
     [string]$email
 )
 
-.\Run-Command.ps1 -RunAsUser 'true' -Command "git config --global user.email ""$email"" "
-.\Run-Command.ps1 -RunAsUser 'true' -Command "git config --global user.name ""$name"" "
+$gitConfigScript = @"
+git config --global user.email "$email"
+git config --global user.name "$name"
+"@
+
+.\Run-Command.ps1 -RunAsUser 'true' -Command $gitConfigScript
 
