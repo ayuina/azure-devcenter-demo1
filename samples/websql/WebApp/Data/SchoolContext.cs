b1e3275f03d6d9fb1254cfde7656316048508f74
ainaba-azure <ainaba@mcap291326.ayuina.moe>
2024-09-27 13:43:14 +0900

    update dbcontext

--- samples/websql/WebApp/Data/SchoolContext.cs
+++ samples/websql/WebApp/Data/SchoolContext.cs
@@ -14,6 +14,15 @@ namespace WebApp.Data
         {
         }
 
-        public DbSet<WebApp.Models.Student> Student { get; set; } = default!;
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
