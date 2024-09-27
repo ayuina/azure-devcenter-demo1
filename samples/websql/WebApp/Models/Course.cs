5620b782a980e44d18e0a46d07ebcb115a2ec4b7
ainaba-azure <ainaba@mcap291326.ayuina.moe>
2024-09-27 13:40:04 +0900

    fix course

--- samples/websql/WebApp/Models/Course.cs
+++ samples/websql/WebApp/Models/Course.cs
@@ -1,17 +1,14 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
 {
-    using System.ComponentModel.DataAnnotations.Schema;
-
-    namespace ContosoUniversity.Models
+    public class Course
     {
-        public class Course
-        {
-            [DatabaseGenerated(DatabaseGeneratedOption.None)]
-            public int CourseID { get; set; }
-            public string Title { get; set; }
-            public int Credits { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
 
-            public ICollection<Enrollment> Enrollments { get; set; }
-        }
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
