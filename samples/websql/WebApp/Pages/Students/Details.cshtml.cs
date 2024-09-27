c60e63b7b0692386b32f3efe456aa5d28b0f0544
ainaba-azure <ainaba@mcap291326.ayuina.moe>
2024-09-27 13:41:53 +0900

    scafolding student

---
+++ samples/websql/WebApp/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApp.Data.SchoolContext _context;
+
+        public DetailsModel(WebApp.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
